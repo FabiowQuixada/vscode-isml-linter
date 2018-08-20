b21b90d5ad412fd639fe168abfef61658a8cc909
Fabiow Quixadá <ftquixada@gmail.com>
2018-08-19 23:34:08 -0300

    Code Reorganization

--- client/src/extension.js
+++ client/src/extension.js
@@ -2,48 +2,49 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
+
 'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
+
 const path = require("path");
-const vscode_1 = require("vscode");
-const vscode_languageclient_1 = require("vscode-languageclient");
+const vscode = require("vscode");
+const vscodeLanguageClient = require("vscode-languageclient");
 let client;
+
+Object.defineProperty(exports, "__esModule", { value: true });
+
 function activate(context) {
-    // The server is implemented in node
-    let serverModule = context.asAbsolutePath(path.join('server', 'src', 'server.js'));
-    // The debug options for the server
-    // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-    let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    let serverOptions = {
-        run: { module: serverModule, transport: vscode_languageclient_1.TransportKind.ipc },
+    
+    const serverModule = context.asAbsolutePath(path.join('server', 'src', 'server.js'));
+    const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+    
+    const serverOptions = {
+        run: { module: serverModule, transport: vscodeLanguageClient.TransportKind.ipc },
         debug: {
             module: serverModule,
-            transport: vscode_languageclient_1.TransportKind.ipc,
+            transport: vscodeLanguageClient.TransportKind.ipc,
             options: debugOptions
         }
     };
-    // Options to control the language client
-    let clientOptions = {
-        // Register the server for plain text documents
+
+    const clientOptions = {
         documentSelector: [{ scheme: 'file', language: 'xml' }],
         synchronize: {
-            // Notify the server about file changes to '.clientrc files contained in the workspace
-            fileEvents: vscode_1.workspace.createFileSystemWatcher('**/.clientrc')
+            fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
         }
     };
-    // Create the language client and start the client.
-    client = new vscode_languageclient_1.LanguageClient('isml-linter', 'Isml Linter', serverOptions, clientOptions);
-    // Start the client. This will also launch the server
+
+    client = new vscodeLanguageClient.LanguageClient('isml-linter', 'Isml Linter', serverOptions, clientOptions);
     client.start();
 }
-exports.activate = activate;
+
 function deactivate() {
+
     if (!client) {
         return undefined;
     }
+
     return client.stop();
 }
+
+exports.activate = activate;
 exports.deactivate = deactivate;
-//# sourceMappingURL=extension.js.map
--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,22 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
+
 'use strict';
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+
+const IsmlLinter = require('isml-linter');
+const vscodeLanguageServer = require("vscode-languageserver");
+
+const connection = vscodeLanguageServer.createConnection(vscodeLanguageServer.ProposedFeatures.all);
+const documents = new vscodeLanguageServer.TextDocuments();
+const defaultSettings = { maxNumberOfProblems: 1000 };
+const globalSettings = defaultSettings;
+const documentSettings = new Map();
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+let hasDiagnosticRelatedInformationCapability = false;
+
+const __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
@@ -11,114 +25,71 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const isml_linter_1 = require('isml-linter');
+
 Object.defineProperty(exports, "__esModule", { value: true });
-const vscode_languageserver_1 = require("vscode-languageserver");
-// Create a connection for the server. The connection uses Node's IPC as a transport.
-// Also include all preview / proposed LSP features.
-let connection = vscode_languageserver_1.createConnection(vscode_languageserver_1.ProposedFeatures.all);
-// Create a simple text document manager. The text document manager
-// supports full document sync only
-let documents = new vscode_languageserver_1.TextDocuments();
-let hasConfigurationCapability = false;
-let hasWorkspaceFolderCapability = false;
-let hasDiagnosticRelatedInformationCapability = false;
-connection.onInitialize((params) => {
-    let capabilities = params.capabilities;
-    // Does the client support the `workspace/configuration` request?
-    // If not, we will fall back using global settings
-    hasConfigurationCapability =
-        capabilities.workspace && !!capabilities.workspace.configuration;
-    hasWorkspaceFolderCapability =
-        capabilities.workspace && !!capabilities.workspace.workspaceFolders;
+
+connection.onInitialize( params => {
+
+    const capabilities = params.capabilities;
+    
+    hasConfigurationCapability = capabilities.workspace && !!capabilities.workspace.configuration;
+    hasWorkspaceFolderCapability = capabilities.workspace && !!capabilities.workspace.workspaceFolders;
     hasDiagnosticRelatedInformationCapability =
         capabilities.textDocument &&
-            capabilities.textDocument.publishDiagnostics &&
-            capabilities.textDocument.publishDiagnostics.relatedInformation;
+        capabilities.textDocument.publishDiagnostics &&
+        capabilities.textDocument.publishDiagnostics.relatedInformation;
+
     return {
         capabilities: {
-            textDocumentSync: documents.syncKind,
-            // Tell the client that the server supports code completion
-            completionProvider: {
-                resolveProvider: true
-            }
+            textDocumentSync: documents.syncKind
         }
     };
 });
-connection.onInitialized(() => {
+
+connection.onInitialized( () => {
     if (hasConfigurationCapability) {
-        // Register for all configuration changes.
-        connection.client.register(vscode_languageserver_1.DidChangeConfigurationNotification.type, undefined);
-    }
-    if (hasWorkspaceFolderCapability) {
-        connection.workspace.onDidChangeWorkspaceFolders(_event => {
-            connection.console.log('Workspace folder change event received.');
-        });
+        connection.client.register(vscodeLanguageServer.DidChangeConfigurationNotification.type, undefined);
     }
 });
-// The global settings, used when the `workspace/configuration` request is not supported by the client.
-// Please note that this is not the case when using this server with the client provided in this example
-// but could happen with other clients.
-const defaultSettings = { maxNumberOfProblems: 1000 };
-let globalSettings = defaultSettings;
-// Cache the settings of all open documents
-let documentSettings = new Map();
-connection.onDidChangeConfiguration(change => {
+
+connection.onDidChangeConfiguration( change => {
+
     if (hasConfigurationCapability) {
-        // Reset all cached document settings
         documentSettings.clear();
     }
     else {
         globalSettings = ((change.settings.ismlLinter || defaultSettings));
     }
-    // Revalidate all open text documents
+
     documents.all().forEach(validateTextDocument);
 });
-function getDocumentSettings(resource) {
-    if (!hasConfigurationCapability) {
-        return Promise.resolve(globalSettings);
-    }
-    let result = documentSettings.get(resource);
-    if (!result) {
-        result = connection.workspace.getConfiguration({
-            scopeUri: resource,
-            section: 'languageServerExample'
-        });
-        documentSettings.set(resource, result);
-    }
-    return result;
-}
-// Only keep settings for open documents
-documents.onDidClose(e => {
+
+documents.onDidClose( e => {
     documentSettings.delete(e.document.uri);
 });
-// The content of a text document has changed. This event is emitted
-// when the text document first opened or when its content has changed.
-documents.onDidChangeContent(change => {
+
+documents.onDidChangeContent( change => {
     validateTextDocument(change.document);
 });
 
 function validateTextDocument(textDocument) {
     return __awaiter(this, void 0, void 0, function* () {
-        // In this simple example we get the settings for every validate run.
-        let settings = yield getDocumentSettings(textDocument.uri);
 
         if (!textDocument.uri.endsWith('.isml')) {
             return;
         }
         
-        // The validator creates diagnostics for all uppercase words length 2 and more
-        let text = textDocument.getText();
-        const result = isml_linter_1.FileParser.parse(text);
-        let diagnostics = [];
+        const text = textDocument.getText();
+        const result = IsmlLinter.FileParser.parse(text);
+        const diagnostics = [];
 
         if (result.errors) {
             for (const brokenRule in result.errors) { 
 
                 result.errors[brokenRule].forEach( function(occurrence) {
 
-                    let diagnosic = {
-                        severity: vscode_languageserver_1.DiagnosticSeverity.Warning,
+                    const diagnosic = {
+                        severity: vscodeLanguageServer.DiagnosticSeverity.Error,
                         range: {
                             start: textDocument.positionAt(occurrence.columnStart),
                             end: textDocument.positionAt(occurrence.columnStart + occurrence.length)
@@ -128,70 +99,12 @@ function validateTextDocument(textDocument) {
 
                     diagnostics.push(diagnosic);
                 });
-                
             }
         }
-        // Send the computed diagnostics to VSCode.
+
         connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
     });
 }
-connection.onDidChangeWatchedFiles(_change => {
-    // Monitored files have change in VSCode
-    connection.console.log('We received an file change event');
-});
-// This handler provides the initial list of the completion items.
-connection.onCompletion((_textDocumentPosition) => {
-    // The pass parameter contains the position of the text document in
-    // which code complete got requested. For the example we ignore this
-    // info and always provide the same completion items.
-    return [
-        {
-            label: 'TypeScript',
-            kind: vscode_languageserver_1.CompletionItemKind.Text,
-            data: 1
-        },
-        {
-            label: 'JavaScript',
-            kind: vscode_languageserver_1.CompletionItemKind.Text,
-            data: 2
-        }
-    ];
-});
-// This handler resolve additional information for the item selected in
-// the completion list.
-connection.onCompletionResolve((item) => {
-    if (item.data === 1) {
-        (item.detail = 'TypeScript details'),
-            (item.documentation = 'TypeScript documentation');
-    }
-    else if (item.data === 2) {
-        (item.detail = 'JavaScript details'),
-            (item.documentation = 'JavaScript documentation');
-    }
-    return item;
-});
-/*
-connection.onDidOpenTextDocument((params) => {
-    // A text document got opened in VSCode.
-    // params.uri uniquely identifies the document. For documents store on disk this is a file URI.
-    // params.text the initial full content of the document.
-    connection.console.log(`${params.textDocument.uri} opened.`);
-});
-connection.onDidChangeTextDocument((params) => {
-    // The content of a text document did change in VSCode.
-    // params.uri uniquely identifies the document.
-    // params.contentChanges describe the content changes to the document.
-    connection.console.log(`${params.textDocument.uri} changed: ${JSON.stringify(params.contentChanges)}`);
-});
-connection.onDidCloseTextDocument((params) => {
-    // A text document got closed in VSCode.
-    // params.uri uniquely identifies the document.
-    connection.console.log(`${params.textDocument.uri} closed.`);
-});
-*/
-// Make the text document manager listen on the connection
-// for open, change and close text document events
+
 documents.listen(connection);
-// Listen on the connection
 connection.listen();
-//# sourceMappingURL=server.js.map

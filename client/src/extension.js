bf4ed81c7bbaa3485d50596951917e56541eeb59
Fabiow Quixadá <ftquixada@gmail.com>
2021-07-26 00:03:32 -0300

    ESLint Fixes

--- client/src/extension.js
+++ client/src/extension.js
@@ -3,24 +3,22 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-'use strict';
-
-const path = require("path");
-const vscode = require("vscode");
-const vscodeLanguageClient = require("vscode-languageclient");
+const path                 = require('path');
+const vscode               = require('vscode');
+const vscodeLanguageClient = require('vscode-languageclient');
 let client;
 
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, '__esModule', { value: true });
 
 function activate(context) {
 
     // TODO: This is not the best way, but that's the way the isml-linter package will understand
     // it's running in a production environment. Will work on a better solution soon;
     process.env.NODE_ENV = 'prod';
-    
+
     const serverModule = context.asAbsolutePath(path.join('server', 'src', 'server.js'));
     const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
-    
+
     const serverOptions = {
         run: { module: serverModule, transport: vscodeLanguageClient.TransportKind.ipc },
         debug: {
@@ -50,5 +48,5 @@ function deactivate() {
     return client.stop();
 }
 
-exports.activate = activate;
+exports.activate   = activate;
 exports.deactivate = deactivate;
